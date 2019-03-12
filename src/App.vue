c67b64e0ad6ee3dffa33c19f92c1aeb0df5f4233
Douglas Lira <douglas.lira.web@gmail.com>
2019-03-12 14:17:39 -0300

    Enable sass

--- src/App.vue
+++ src/App.vue
@@ -13,24 +13,26 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    @import './assets/sass/var';
 
-.slide-fade-enter-active {
-  transition: all .2s ease;
-}
-.slide-fade-leave-active {
-  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-active {
-  padding-left: 10px;
-  opacity: 0;
-}
+    #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+    }
+
+    .slide-fade-enter-active {
+      transition: all .2s ease;
+    }
+    .slide-fade-leave-active {
+      transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-active {
+      padding-left: 10px;
+      opacity: 0;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App'
 import router from './router'
 import filter from './utils/filter'
 import store from './store'
+import Buefy from 'buefy'
+import '../node_modules/buefy/dist/buefy.css'
 
+Vue.use(Buefy)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
